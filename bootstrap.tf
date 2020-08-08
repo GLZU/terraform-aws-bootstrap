b7fd682663d84ca80bd5f092cf7bde1db853b764
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-08 02:16:36 +0000

    Update bootstrap.tf

--- bootstrap.tf
+++ bootstrap.tf
@@ -54,7 +54,7 @@ resource "tfe_variable" "test" {
   key          = "my_key_name"
   value        = "my_value_name"
   category     = "terraform"
-  workspace_id = "${tfe_workspace.test.id}"
+  workspace_id = tfe_workspace.test.id
   description  = "a useful description"
   provider     = tfe.tfe1
 }
