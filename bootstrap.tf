e7e81257510fe04a69a5373e3ae1b668dbf1099f
udayanms <52168703+udayanms@users.noreply.github.com>
2020-08-09 17:14:18 +0000

    Update bootstrap.tf

--- bootstrap.tf
+++ bootstrap.tf
@@ -41,7 +41,7 @@ provider "github" {
 
 module "modules" {
   source  = "10.1.199.170/TFOLZU/modules/aws/LandingZone/mod_workspace"
-  version = "0.1.0"
+#  version = "0.1.0"
   params = local.params
   providers = {
      github.github1 = github.github1
